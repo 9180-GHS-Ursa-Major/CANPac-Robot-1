3b8aff34b59b875f037bce85d49e6c5f3082d6b4
9180 GHS Ursa Major <ghs.ursa.major@gmail.com>
2024-02-21 16:09:40 -0800

    Comments

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -23,6 +23,7 @@ public class ArmSubsystem extends SubsystemBase {
     }
 
     //Control the arm at a certain speed
+    //Same problem as before - arm angle isn't normal zero - it's 0 is closer to the angle of the supporting arm (I want to say 16 degrees lower)
     public void armControl(double speed) {
          armMotor1.set(0.25 * speed);
         
